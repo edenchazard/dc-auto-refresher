4a53393decbf613638831af450110b115f95ab27
Charley Reeson <neon.fortytwo@gmail.com>
2022-02-02 23:39:35 +0000

    feature: cycling favicon when auto refreshing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import DragonTR from "./components/DragonTR";
 import AddDragon from './components/AddDragon';
 import RefresherControls from './components/RefresherControls';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { isCodeInList, validateCode } from "./functions";
 import { Dragon } from "./interfaces";
 
@@ -10,6 +10,44 @@ export default function App() {
     const   [listOfDragons, setListOfDragons] = useState<Dragon[]>([]),
             [rate, setRate] = useState<number>(250),
             [autorefresh, setAutorefresh] = useState<boolean>(false);
+
+    const curIconCycle = useRef(0);
+    const iconInterval = useRef(null);
+
+    // Code to cycle the favicon
+    useEffect(() =>{
+        if(autorefresh === true){
+            iconInterval.current = window.setInterval(() => {
+                changeIcon();
+            }, 1000);
+        }
+        else{
+            window.clearInterval(iconInterval.current);
+            iconInterval.current = null;
+            // At some point when I have an actual favicon,
+            // revert it back to that
+        }
+    }, [autorefresh]);
+
+    function changeIcon(){
+        const getCycle = () => {
+            curIconCycle.current += 1;
+
+            if(listOfDragons[curIconCycle.current] === undefined){
+                // reset to 0
+                curIconCycle.current = 0;
+            }
+            const code = listOfDragons[curIconCycle.current].code;
+
+            return `https://dragcave.net/image/${code}.gif`;
+        }
+
+        const oldIcon = document.head.querySelector('link[rel="icon"]');
+        let newIcon = document.createElement('link');
+        newIcon.rel = 'icon';
+        newIcon.href = getCycle();
+        document.head.replaceChild(newIcon, oldIcon);
+    }
 
     function handleAdd(code: string, instances: number){
         // prevent people adding an already added code to the list
@@ -77,7 +115,7 @@ export default function App() {
                                 code={dragon.code}
                                 rate={rate}
                                 instances={dragon.instances}
-                                setInstances={(instances) => handleUpdateDragon(index, instances)}
+                                setInstances={(instances: number) => handleUpdateDragon(index, instances)}
                                 remove={() => handleRemove(index)} />
                         )
                     })
