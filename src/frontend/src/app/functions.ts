dc636569dbff8ab4449ac250ae9b231f05a40dfc
Charley Reeson <neon.fortytwo@gmail.com>
2022-07-04 13:38:39 +0100

    minor fixes - share link and some QS validation/sanitization

--- src/frontend/src/app/functions.ts
+++ src/frontend/src/app/functions.ts
@@ -27,16 +27,20 @@ export function getListFromQS(url: string = window.location.search){
         const dragons = query.get('list').split(';');
         for(const dragon of dragons){
             const
-                data =  dragon.split(','),
+                data = dragon.split(','),
                 code = data[0],
-                instances = (data[1] == null ? null : parseInt(data[1])),
-                tod = (data[2] == null ? null : parseInt(data[2]));
+                // defaults to 0 if not valid or floors if float
+                instances = ~~data[1],
+                // check if tod has passed and null it if so
+                // or a 'bad' value
+                tod = (Date.now() > parseInt(data[2]) || isNaN(data[2] as any) ? null : parseInt(data[2]));
             
-            // todo: validate each value
-            unserialize.push({ code, instances, tod });
+            // skip if not valid code 
+            if(validateCode && !isCodeInList(unserialize, code))
+                unserialize.push({ code, instances, tod });
         }
 
-        window.history.replaceState({list: ''}, '', '/dc/auto-refresher/');
+        window.history.replaceState({list: ''}, '', window.location.pathname);
     }
 
     return (unserialize.length > 0 ? unserialize : null);
@@ -46,8 +50,7 @@ export function createShareLinkFromList(list: Dragon[]){
     const map = ({ code, instances, tod }) => 
         `${code},${instances},${(isNaN(tod) ? 'null' : tod )}`;
 
-    return window.location.href + "?list=" +
-        list
-            .map(map)
-            .join(';');
+    const { origin, pathname } = window.location;
+
+    return `${origin}${pathname}?list=`+list.map(map).join(';');
 }
