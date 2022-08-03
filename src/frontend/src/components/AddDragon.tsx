4116136fa12616b7f96535839f4bcbf79677cc62
Charley Reeson <neon.fortytwo@gmail.com>
2022-08-03 09:39:23 +0100

    turn add dragon into form

--- src/frontend/src/components/AddDragon.tsx
+++ src/frontend/src/components/AddDragon.tsx
@@ -8,7 +8,8 @@ export default function AddDragon({addToList, rate}){
             [instances, setInstances] = useState<number>(1),
             [tod, setTOD] = useState<string>(null);
 
-    function handleAdd(){
+    function handleAdd(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
         // by default don't specify a tod, only send the clock value
         // if it's been modified
         let s = null;
@@ -26,51 +27,56 @@ export default function AddDragon({addToList, rate}){
 
     return (
             <div className='space-y-1 my-1'>
-                <div className='flex justify-between items-center'>
-                    <label htmlFor='code'>Code:</label>
-                    <input
-                        id="code"
-                        className='text-black'
-                        type='text'
-                        placeholder="Code"
-                        value={code}
-                        size={5}
-                        minLength={4}
-                        maxLength={5}
-                        onChange={(e) => setCode(e.target.value)}/>
-                </div>
-                <div className='flex justify-between items-center'>
-                    <label htmlFor='instances'>Instances:</label>
-                    <DragonInstancesInput
-                        instances={instances}
-                        setInstances={setInstances} />
-                </div>
-                <div className='flex justify-between text-gray-400'>
-                    <p>
-                        {
-                        (rate === 0
-                            ? "Variable: Each image will reload as soon as it has loaded."
-                            : `~${(60000 / rate) * instances} views per minute: Actual rate depends on different factors.`
-                         )+ " Specify '0' to add the dragon but not auto-refresh it."
-                        }
-                    </p>
-                </div>
-                <div className='flex justify-between items-center'>
-                    <label htmlFor='tod'>Dies at:</label>
-                    <TimePicker
-                        id='tod'
-                        onChange={setTOD}
-                        value={tod}
-                        format="mm:ss"
-                        maxDetail='second'
-                        disableClock={true} />
-                </div>
-                <div className='flex justify-between text-gray-400'>
-                    <p>Specify the minute and second the "will die if... " changes and FART will provide a TOD countdown. Dragon must be unfogged.</p>
-                </div>
-                <div className='flex items-end flex-col'>
-                    <button className="rounded bg-indigo-500 px-2 py-1 hover:bg-indigo-700" onClick={handleAdd}>Add</button>
-                </div>
+                <form
+                    onSubmit={handleAdd}>
+                    <div className='flex justify-between items-center space-y-1'>
+                        <label htmlFor='code'>Code:</label>
+                        <input
+                            id="code"
+                            className='text-black'
+                            type='text'
+                            placeholder="Code"
+                            value={code}
+                            size={5}
+                            minLength={4}
+                            maxLength={5}
+                            onChange={(e) => setCode(e.target.value)}/>
+                    </div>
+                    <div className='flex justify-between items-center space-y-1'>
+                        <label htmlFor='instances'>Instances:</label>
+                        <DragonInstancesInput
+                            instances={instances}
+                            setInstances={setInstances} />
+                    </div>
+                    <div className='flex justify-between text-gray-400 space-y-1'>
+                        <p>
+                            {
+                            (rate === 0
+                                ? "Variable: Each image will reload as soon as it has loaded."
+                                : `~${(60000 / rate) * instances} views per minute: Actual rate depends on different factors.`
+                            )+ " Specify '0' to add the dragon but not auto-refresh it."
+                            }
+                        </p>
+                    </div>
+                    <div className='flex justify-between items-center'>
+                        <label htmlFor='tod'>Dies at:</label>
+                        <TimePicker
+                            id='tod'
+                            onChange={setTOD}
+                            value={tod}
+                            format="mm:ss"
+                            maxDetail='second'
+                            disableClock={true} />
+                    </div>
+                    <div className='flex justify-between text-gray-400'>
+                        <p>Specify the minute and second the "will die if... " changes and FART will provide a TOD countdown. Dragon must be unfogged.</p>
+                    </div>
+                    <div className='flex items-end flex-col'>
+                        <button
+                            className="rounded bg-indigo-500 px-2 py-1 hover:bg-indigo-700"
+                            type="submit">Add</button>
+                    </div>
+                </form>
             </div>
     );
 }
