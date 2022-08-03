f9194773c28eecae931247c5aae5eb5a092cb34b
Charley Reeson <neon.fortytwo@gmail.com>
2022-08-03 09:39:35 +0100

    required placeholder attribute

--- src/frontend/src/components/DragonInstancesInput.tsx
+++ src/frontend/src/components/DragonInstancesInput.tsx
@@ -6,6 +6,7 @@ export default function DragonInstancesInput({instances, setInstances}){
             min="0"
             max="10"
             size={2}
+            placeholder="instances"
             onChange={(e) => setInstances(parseInt(e.target.value))}
             className='text-black' />
     )
