25c8e5648f028223d78b5f1c79f59a8920058682
Charley Reeson <neon.fortytwo@gmail.com>
2022-10-01 16:00:25 +0100

    add github url to footer

--- src/frontend/src/components/Footer.tsx
+++ src/frontend/src/components/Footer.tsx
@@ -1,16 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+/*import { faPaypal } from "@fortawesome/free-brands-svg-icons";
+
+
+                    <a
+                        className="mx-2"
+                        href="https://github.com/edenchazard/dc-auto-refresher/"
+                        title="Donate via PayPal">
+                        <FontAwesomeIcon size="2x" icon={faPaypal as IconProp} />
+                    </a>
+*/
 export default function Footer(){
     return (
         <div className='p-2 mt-2 text-white text-center'>
-            <header>
-                FART
-            </header>
             <footer>
-                <div>
-                    Part of <a
-                        className='underline'
-                        href='/dc/tools'>Chazza's DC Tools</a>
+                <div className="flex flex-row justify-center items-center">
+                    <a
+                        className="mx-2"
+                        href="https://github.com/edenchazard/dc-auto-refresher/"
+                        title="Github">
+                        <FontAwesomeIcon size="2x" icon={faGithub as IconProp} />
+                    </a>
                 </div>
-                <div>v{process.env.REACT_APP_VERSION} &copy; eden chazard</div>
+                <div className="m-2">
+                    <div>
+                        <span>FART</span><br />
+                        Part of <a
+                            className='underline'
+                            href='/dc/tools'>Chazza's DC Tools</a>
+                    </div>
+                    <div>v{process.env.REACT_APP_VERSION} &copy; eden chazard</div>
+                </div>
             </footer>
         </div>
     );
