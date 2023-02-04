7baecdcb622595cc6a6fd72d73ae4215bc24726b
Charley Reeson <neon.fortytwo@gmail.com>
2023-02-04 20:30:45 +0000

    add eslint and prettier configs

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -2,120 +2,144 @@
 import config from './appConfig';
 import Koa from 'koa';
 import Router from '@koa/router';
-import axios from "axios";
-import { APIDragon, Dragon, DragonInferredDetails } from './types';
+import axios from 'axios';
+import type { APIDragon, APIResponse, DragonInferredDetails } from './types';
 
 const app = new Koa();
 const router = new Router({ prefix: config.apiPath });
 
-function validateCode(code: string) {
-    return /^[a-zA-Z0-9]{5}$/.test(code);
+function validateCode(code: string): boolean {
+  return /^[a-zA-Z0-9]{5}$/.test(code);
 }
 
 // Function to validate API response from dragcave
 // Only checks the fields we need.
-function validAPIDragon(responseObj: APIDragon){
-    let pass = true;
-    ['grow', 'death', 'start', 'hatch', 'hoursleft'].forEach((prop) => {
-        if(prop in responseObj === false)
-            pass = false;
-    });
+function validAPIDragon(responseObj: APIDragon): boolean {
+  let pass = true;
+  ['grow', 'death', 'start', 'hatch', 'hoursleft'].forEach((prop) => {
+    if (!(prop in responseObj)) {
+      pass = false;
+    }
+  });
 
-    return pass;
+  return pass;
 }
 
 // Determines various characteristics about a dragon
-function determineDragonDetails(dragon: APIDragon, seconds: number){
-    // We need to compare the current seconds passed this hour with that of
-    // what the user inputted, then we set the tod to the current date +
-    // the hours, mins and seconds of the dragon remaining
-    const calculateTOD = (hoursLeft: number, seconds: number) => {
-        if(hoursLeft == -1) return null;
+function determineDragonDetails(
+  dragon: APIDragon,
+  seconds: number,
+): DragonInferredDetails {
+  // We need to compare the current seconds passed this hour with that of
+  // what the user inputted, then we set the tod to the current date +
+  // the hours, mins and seconds of the dragon remaining
+  const calculateTOD = (hoursLeft: number, seconds: number): number | null => {
+    if (hoursLeft === -1) return null;
 
-        const now = new Date();
-        const a = (now.getUTCMinutes() * 60) + (now.getUTCSeconds());
+    const now = new Date();
+    const a = now.getUTCMinutes() * 60 + now.getUTCSeconds();
 
-        // is hoursleft rounded? let's find out
-        const hours = now.getUTCHours() + hoursLeft;
-        const roundedHours = a >= seconds ? hours : hours - 1;
+    // is hoursleft rounded? let's find out
+    const hours = now.getUTCHours() + hoursLeft;
+    const roundedHours = a >= seconds ? hours : hours - 1;
 
-        // create the date in the future
-        const tod = new Date();
-        tod.setUTCHours(roundedHours, Math.floor(seconds / 60), seconds % 60);
+    // create the date in the future
+    const tod = new Date();
+    tod.setUTCHours(roundedHours, Math.floor(seconds / 60), seconds % 60);
 
-        //console.log('user specified date ' + tod);
+    // console.log('user specified date ' + tod);
 
-        return tod.getTime();
-    }
+    return tod.getTime();
+  };
 
-    const
-        isAdult = dragon.grow != 0,
-        isDead = dragon.death !== "0",
-        isHidden = dragon.start === "0" && dragon.hatch === "0" && dragon.death === "0",
-        isFrozen = !isAdult && !isDead && !isHidden && dragon.hoursleft == -1,
-        justHatched = dragon.hoursleft === 168 && dragon.hatch !== "0";
+  const isAdult = dragon.grow !== '0';
+  const isDead = dragon.death !== '0';
+  const isHidden =
+    dragon.start === '0' && dragon.hatch === '0' && dragon.death === '0';
+  const isFrozen = !isAdult && !isDead && !isHidden && dragon.hoursleft === -1;
+  const justHatched = dragon.hoursleft === 168 && dragon.hatch !== '0';
 
-    const tod = (seconds !== null ? calculateTOD(dragon.hoursleft, seconds) : null);
+  const tod = seconds !== null ? calculateTOD(dragon.hoursleft, seconds) : null;
 
-    const inferred: DragonInferredDetails = { isAdult, isDead, isHidden, isFrozen, justHatched, tod };
-    return inferred;
+  const inferred: DragonInferredDetails = {
+    isAdult,
+    isDead,
+    isHidden,
+    isFrozen,
+    justHatched,
+    tod,
+  };
+  return inferred;
+}
+
+class APIError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'APIError';
+  }
 }
 
 router.get('/check/:code', async (ctx) => {
-    const code: string = ctx.params.code;
+  const code: string = ctx.params.code;
 
-    // force a number or make it null
-    const seconds = Number(ctx.query.tod); 
+  // force a number or make it null
+  const seconds = Number(ctx.query.tod);
 
-    if(!validateCode(code)){
-        ctx.body = { 
-            errors: true,
-            errorMessage: "The code isn't valid."
-        };
-        return;
-    }
+  if (!validateCode(code)) {
+    // todo maybe this should be changed, as it's not really an API error.
+    throw new APIError("The code isn't valid.");
+  }
 
-    const dcAPI = await axios(config.DCAPIURL+"/view/"+code);
-    const response: APIDragon = dcAPI.data?.dragons?.[code];
+  const dcAPI = await axios.get<APIResponse>(config.DCAPIURL + '/view/' + code);
+  const response = dcAPI.data;
 
-    if(!response){
-        ctx.body = { 
-            errors: true,
-            errorMessage: "The dragon doesn't exist or there's an issue with dragcave.net."
-        };
-        return;
-    }
+  if (response.errors.length > 0) {
+    throw new APIError(
+      "The dragon doesn't exist or there's an issue with dragcave.net.",
+    );
+  }
 
-    if(!validAPIDragon(response))
-        throw new Error("The API response from dragcave.net isn't valid.");
+  if (!validAPIDragon(response.dragons[code])) {
+    throw new APIError("The dragon data received isn't valid.");
+  }
 
-    // extra info about the dragon based on response
-    const dragon: Dragon = { ...response, ...determineDragonDetails(response, seconds) };
-    ctx.body = {
-        errors: false,
-        acceptable: !dragon.isAdult && !dragon.isDead && !dragon.isFrozen,
-        justHatched: dragon.justHatched,
-        tod: dragon.tod
-    };
+  // extra info about the dragon based on response
+  const dragon = {
+    ...response,
+    ...determineDragonDetails(response.dragons[code], seconds),
+  };
+
+  ctx.body = {
+    errors: false,
+    acceptable: !dragon.isAdult && !dragon.isDead && !dragon.isFrozen,
+    justHatched: dragon.justHatched,
+    tod: dragon.tod,
+  };
 });
 
 app
-    .use(async (ctx, next) => {
-        try {
-            await next();
-        }
-        // TODO: I don't like the any but ts complains if it isn't
-        catch(err: any) {
-            console.log(err)
-            ctx.status = err.status || 500;
-            ctx.body = { errors: true, errorMessage: err.message };
-        }
-    })
-    .use(router.routes())
-    .use(router.allowedMethods())
-    .listen(config.port);
+  .use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err: any) {
+      ctx.status = 500;
+      if (err instanceof APIError) {
+        ctx.body = { errors: true, errorMessage: err.message };
+      } else {
+        // all other errors
+        ctx.body = {
+          errors: true,
+          errorMessage: 'Sorry, an error has occurred.',
+        };
+      }
+      console.log(err);
+    }
+  })
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(config.port);
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
--- src/backend/src/types.ts
+++ src/backend/src/types.ts
@@ -1,41 +1,47 @@
 interface APIDragon {
-    id: string,
-    name: string | null,
-    owner: string,
-    start: string,
-    hatch: string,
-    grow: number,
-    death: string,
-    views: number,
-    unique: number,
-    clicks: number,
-    gender: "" | "Male" | "Female",
-    hoursleft: number,
-    parent_f: string,
-    parent_m: string
+  id: string;
+  name: string | null;
+  owner: string;
+  start: string;
+  hatch: string | number;
+  grow: string | number;
+  death: string | number;
+  views: number;
+  unique: number;
+  clicks: number;
+  gender: '' | 'Male' | 'Female';
+  hoursleft: number;
+  parent_f: string;
+  parent_m: string;
+}
+
+interface APIResponse {
+  errors: Array<[number, string]>;
+  dragons: Record<string, APIDragon>;
 }
 
 interface DragonInferredDetails {
-    isAdult: boolean,
-    isDead: boolean,
-    isHidden: boolean,
-    isFrozen: boolean,
-    justHatched: boolean,
-    tod: number | null
+  isAdult: boolean;
+  isDead: boolean;
+  isHidden: boolean;
+  isFrozen: boolean;
+  justHatched: boolean;
+  tod: number | null;
 }
 
 type Dragon = APIDragon & DragonInferredDetails;
 
 interface AppConfig {
-    port: number,
-    apiPath: string,
-    DCAPIURL: string,
-    defaultError: { status: number, message: string }
+  port: number;
+  apiPath: string;
+  DCAPIURL: string;
+  defaultError: { status: number; message: string };
 }
 
-export {
-    APIDragon,
-    DragonInferredDetails,
-    Dragon,
-    AppConfig
-}
+export type {
+  APIDragon,
+  DragonInferredDetails,
+  Dragon,
+  AppConfig,
+  APIResponse,
+};
