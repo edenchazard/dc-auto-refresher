583b24bf6a833a3fc895e288b92f08a61d353afd
edenchazard <neon.fortytwo@gmail.com>
2026-04-05 15:48:21 +0100

    update react deps

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,8 +10,8 @@ export default defineConfig({
   trailingSlash: 'never',
   output: 'server',
   adapter: node({ mode: 'standalone' }),
-  integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [react()],
 });
