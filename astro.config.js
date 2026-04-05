dd02c9c7d44dd9e1c35e4858931f04501763aac5
edenchazard <neon.fortytwo@gmail.com>
2026-04-05 16:06:15 +0100

    rm postcss

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
